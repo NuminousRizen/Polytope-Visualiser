458ebe3e8d7448d73fc94fadf3e5170695bd8e69
Ramanpreet Singh <57103998+NuminousRizen@users.noreply.github.com>
2021-11-10 14:04:38 +0000

    Edit PolytopeUI for easier testing
    Pressing spacebar will make a new polytope; just using this for easier testing.
    
    Also fixed typo in toDisplay.

--- Polytope Visualiser/Assets/Scripts/2D Polytope/UI/PolytopeUI.cs
+++ Polytope Visualiser/Assets/Scripts/2D Polytope/UI/PolytopeUI.cs
@@ -14,28 +14,38 @@ namespace _2D_Polytope.UI
         public List<Vector2> points;
         private List<Vector2> convexHullPoints;
 
-        private void Start()
+        void Update()
         {
-            Camera camera = Camera.main;
-            float height = 2f * camera.orthographicSize;
-            float width = height * camera.aspect;
-            float buffer = height * .05f;
-            
-            points = new List<Vector2>();
-
-            int numberOfPoints = Random.Range(2, 51);
-            Debug.Log("Making a polytope with " + numberOfPoints + " points.");
-
-            for (int i = 0; i < numberOfPoints; i++)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                points.Add(new Vector2(Random.Range(-(width / 2) + buffer, (width / 2) - buffer), Random.Range(-(height / 2) + buffer, (height / 2) - buffer)));
+                foreach (Transform child in transform) {
+                    Destroy(child.gameObject);
+                }
+                
+                Camera camera = Camera.main;
+                float height = 2f * camera.orthographicSize;
+                float width = height * camera.aspect;
+                float buffer = height * .05f;
+            
+                points = new List<Vector2>();
+
+                int numberOfPoints = Random.Range(2, 51);
+                Debug.Log("Making a polytope with " + numberOfPoints + " points.");
+
+                for (int i = 0; i < numberOfPoints; i++)
+                {
+                    points.Add(new Vector2(Random.Range(-(width / 2) + buffer, (width / 2) - buffer), Random.Range(-(height / 2) + buffer, (height / 2) - buffer)));
+                }
+            
+                BuildPolytope();
             }
-            BuildPolytope();
         }
 
         void BuildPolytope()
         {
-            convexHullPoints = GiftWrap.FindConvexHull(points);
+            // convexHullPoints = GiftWrap.FindConvexHull(points);
+            convexHullPoints = GrahamScan.GetConvexHull(points);
+            
             foreach (Vector2 point in points)
             {
                 Transform circle = Instantiate(pointPrefab, point, transform.rotation);
@@ -54,15 +64,15 @@ namespace _2D_Polytope.UI
             lineRenderer.startWidth = theme.lineSize;
             lineRenderer.endWidth = theme.lineSize;
             lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-            List<Vector3> toDiplay = new List<Vector3>();
+            List<Vector3> toDisplay = new List<Vector3>();
             foreach (Vector2 point in convexHullPoints)
             {
-                toDiplay.Add(new Vector3(point.x, point.y));
+                toDisplay.Add(new Vector3(point.x, point.y));
             }
-            toDiplay.Add(new Vector3(convexHullPoints[0].x, convexHullPoints[0].y));
+            toDisplay.Add(new Vector3(convexHullPoints[0].x, convexHullPoints[0].y));
             
-            lineRenderer.positionCount = toDiplay.Count;
-            lineRenderer.SetPositions(toDiplay.ToArray());
+            lineRenderer.positionCount = toDisplay.Count;
+            lineRenderer.SetPositions(toDisplay.ToArray());
         }
     }
 }
