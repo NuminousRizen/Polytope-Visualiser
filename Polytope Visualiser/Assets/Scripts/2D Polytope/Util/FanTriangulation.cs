60b740dcfd993a18d2a62a067ef00411740870ec
Ramanpreet Singh <57103998+NuminousRizen@users.noreply.github.com>
2021-11-12 18:18:50 +0000

    Add Fan Triangulation
    This algorithm will be used to triangulate a custom mesh when given a list of points (points must be given in anti-clockwise order).

---
+++ Polytope Visualiser/Assets/Scripts/2D Polytope/Util/FanTriangulation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _2D_Polytope.Util
+{
+    public static class FanTriangulation
+    {
+        public static int[] Triangulate(List<Vector2> points)
+        {
+            List<int> triangles = new List<int>();
+            if (points.Count < 4)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    triangles.Add(i);
+                }
+
+                return triangles.ToArray();
+            }
+
+            for (int i = 2; i < points.Count; i++)
+            {
+                triangles.Add(0);
+                triangles.Add(i);
+                triangles.Add(i - 1);
+            }
+
+            return triangles.ToArray();
+        }
+    }
+}
