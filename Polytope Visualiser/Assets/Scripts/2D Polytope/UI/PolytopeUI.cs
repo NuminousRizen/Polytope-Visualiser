1f7277ebe654dc9f506db78529ceb47a30d725fe
Ramanpreet Singh <57103998+NuminousRizen@users.noreply.github.com>
2021-11-12 18:22:28 +0000

    Add custom mesh generation
    Added custom mesh generation that uses the convex hull points and the triangulation algorithm to create a custom mesh, which is then assigned to a new game object called "polytope".

--- Polytope Visualiser/Assets/Scripts/2D Polytope/UI/PolytopeUI.cs
+++ Polytope Visualiser/Assets/Scripts/2D Polytope/UI/PolytopeUI.cs
@@ -56,7 +56,7 @@ namespace _2D_Polytope.UI
                     : theme.pointColour;
                 circle.localScale = Vector3.one * theme.pointSize;
             }
-
+            
             LineRenderer lineRenderer = new GameObject("Lines").AddComponent<LineRenderer>();
             lineRenderer.transform.parent = transform;
             lineRenderer.startColor = theme.lineColour;
@@ -70,9 +70,21 @@ namespace _2D_Polytope.UI
                 toDisplay.Add(new Vector3(point.x, point.y));
             }
             toDisplay.Add(new Vector3(convexHullPoints[0].x, convexHullPoints[0].y));
-            
             lineRenderer.positionCount = toDisplay.Count;
             lineRenderer.SetPositions(toDisplay.ToArray());
+            
+            toDisplay.RemoveAt(toDisplay.Count - 1);
+            Mesh polytopeMesh = new Mesh();
+            polytopeMesh.vertices = toDisplay.ToArray();
+            polytopeMesh.triangles = FanTriangulation.Triangulate(convexHullPoints);
+            polytopeMesh.RecalculateNormals();
+            
+            GameObject polytope = new GameObject("Polytope");
+            polytope.AddComponent<MeshFilter>().mesh = polytopeMesh;
+            MeshRenderer polytopeRenderer = polytope.AddComponent<MeshRenderer>();
+            polytopeRenderer.sharedMaterial = new Material(Shader.Find("Sprites/Default"));
+            polytopeRenderer.sharedMaterial.color = Color.white;
+            polytope.transform.parent = transform;
         }
     }
 }
