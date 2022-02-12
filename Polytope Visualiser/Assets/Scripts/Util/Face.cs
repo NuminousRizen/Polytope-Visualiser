54cf815011a4e59c4f73558d27200e5d7dd3abca
Ramanpreet Singh <57103998+NuminousRizen@users.noreply.github.com>
2022-02-12 23:07:53 +0000

    Add documentation for faces.

--- Polytope Visualiser/Assets/Scripts/Util/Face.cs
+++ Polytope Visualiser/Assets/Scripts/Util/Face.cs
@@ -2,6 +2,9 @@
 
 namespace Util
 {
+    /// <summary>
+    /// An equality comparer for faces.
+    /// </summary>
     public class FaceEqualityComparer : IEqualityComparer<Face>
     {
         public bool Equals(Face f1, Face f2)
@@ -41,10 +44,15 @@ namespace Util
         }
     }
     
+    /// <summary>
+    /// A face is defined as a triangle made of three points and three edges.
+    /// </summary>
     public struct Face
     {
         private VectorD3D p1, p2, p3;
         private Edge edge1, edge2, edge3;
+        
+        // Holds a 3D inequality (mainly to not have to generate it everytime we need to check visibility).
         private PlaneInequality faceInequality;
 
         public Face(VectorD3D p1, VectorD3D p2, VectorD3D p3)
@@ -109,12 +117,21 @@ namespace Util
         {
             return "Point1: " + p1 + "\nPoint2" + p2 + "\nPoint3" + p3;
         }
-
+        
+        /// <summary>
+        /// Get the distance between this face (plane) and a given point.
+        /// </summary>
+        /// <param name="point">The point from which to calculate the distance.</param>
+        /// <returns>The distance from the point to this face.</returns>
         public double GetDistanceFromFacePlane(VectorD3D point)
         {
             return faceInequality.GetDistance(point);
         }
 
+        /// <summary>
+        /// A function to correct the normal of this face, i.e. set which way this face is pointing to.
+        /// </summary>
+        /// <param name="internalPoints">Some reference points that are on the opposite side to which this face should be pointing in.</param>
         public void CorrectNormal(List<VectorD3D> internalPoints)
         {
             foreach (VectorD3D point in internalPoints)
@@ -132,21 +149,38 @@ namespace Util
             }
         }
 
+        /// <summary>
+        /// Check whether this face is visible from a given point.
+        /// </summary>
+        /// <param name="eyePoint">The point from which to check the visibility from.</param>
+        /// <returns>Whether this face is visible from the eye point.</returns>
         public bool IsVisible(VectorD3D eyePoint)
         {
             return faceInequality.GetDistance(eyePoint) > VectorD3D.GetEpsilon();
         }
 
+        /// <summary>
+        /// Get the inequality representation for this face.
+        /// </summary>
+        /// <returns>The inequality representation of this face.</returns>
         public string GetInequality()
         {
             return faceInequality.ToString();
         }
 
+        /// <summary>
+        /// Get the edges that make up this face.
+        /// </summary>
+        /// <returns>The edges that make up this face.</returns>
         public (Edge, Edge, Edge) GetEdges()
         {
             return (edge1, edge2, edge3);
         }
 
+        /// <summary>
+        /// Get the points that make up this face.
+        /// </summary>
+        /// <returns>The points that make up this face.</returns>
         public (VectorD3D, VectorD3D, VectorD3D) GetPoints()
         {
             return (p1, p2, p3);
